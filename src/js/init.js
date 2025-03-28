f66a5b23c35455326a3e95f1bbc1e179e6e84ca1
antony.sl <antony.g.s.lopez@gmail.com>
2025-03-28 02:57:28 -0500

    Agregado de modal inicio (prueva) y correccion de mostrar cartas, etapa 1/3 falta sistema de puntos funcion de voltear carta modal de puntuacones

--- src/js/init.js
+++ src/js/init.js
@@ -1,12 +1,33 @@
 //contendor de categorias.
 const tematicas = {
     Arbol: [
-        "src/img/categorias/Arbol/tree1.png",
-        "src/img/categorias/Arbol/tree2.png",
-        "src/img/categorias/Arbol/tree3.png",
-        "src/img/categorias/Arbol/tree4.png"
+        "src/img/categorias/Arboles/tree1.png",
+        "src/img/categorias/Arboles/tree2.png",
+        "src/img/categorias/Arboles/tree3.png",
+        "src/img/categorias/Arboles/tree4.png",
+        "src/img/categorias/Arboles/tree5.png",
+        "src/img/categorias/Arboles/tree6.png",
+        "src/img/categorias/Arboles/tree7.png",
+        "src/img/categorias/Arboles/tree8.png",
+        "src/img/categorias/Arboles/tree9.png",
+        "src/img/categorias/Arboles/tree10.png",
+        "src/img/categorias/Arboles/tree11.png",
+        "src/img/categorias/Arboles/tree12.png",
+        "src/img/categorias/Arboles/tree13.webp",
+        "src/img/categorias/Arboles/tree14.webp",
+        "src/img/categorias/Arboles/tree15.webp",
+        "src/img/categorias/Arboles/tree16.webp",
+        "src/img/categorias/Arboles/tree17.webp",
+        "src/img/categorias/Arboles/tree18.webp",
+        "src/img/categorias/Arboles/tree19.webp",
+        "src/img/categorias/Arboles/tree20.webp",
     ]
 };
+
+const avatares = [
+    "src/img/perfil-Madara.webp",
+    "src/img/perfil-zerotwo.jpg"
+]
 
 //funciones universales
 
@@ -34,7 +55,7 @@ const tematicas = {
             } else {
                 clearInterval(contador);
                 console.log(`Tiempo detenido`);
-                
+                mostrarQuitarModal(modalGameOver);                
             }
         },1000);
     }
@@ -60,12 +81,6 @@ const tematicas = {
         }
     }
 
-    //funcnion show/hidden playpause
-    function playPauseShowHidden(bool) {
-        const cronometro = document.getElementById("cronometro");
-        bool? (cronometro.onclick = null) : (cronometro.onclick = function () {playPause(this)});
-    }
-
 //arreglo EJEMPLO para exponer modal GameOver
 const resultados = {
     points: 350,
@@ -75,12 +90,21 @@ const resultados = {
 }
 
 // Obteniendo objetos para trabajar main
+
+    function RandomPorcantidad(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // Índice aleatorio entre 0 e i
+            [array[i], array[j]] = [array[j], array[i]]; // Intercambio
+        }
+        return array;
+    }
     //funcion para mostrar cartas
     const tableroGame = document.getElementById("game");
-    function mostrarCartas(valor){
-        const cartas = tematicas[valor].map ((image,index) =>
-            `<img onclick="addcart(this)" src="${image}"  alt="${valor}" data-valor="${index}" class="w-[150px] h-[200px]  shadow-md shadow-[#e9e9e9] border-[#bfe5fb] border-[1px] rounded-2xl hover:scale-[1.03] hover:shadow-black hover:shadow-1xl">`
+    function mostrarCartas(valor, cantidad){
+        const cartas = tematicas[valor].slice(0, cantidad).map ((image,index) =>
+            `<img onclick="addcart(this)" src="${image}"  alt="${valor}" data-valor="${index}" class="w-[150px] h-[200px]  shadow-md shadow-[#e9e9e9] border-[#bfe5fb] border-[1px] rounded-2xl hover:scale-[1.03] hover:shadow-black hover:shadow-1xl"><img onclick="addcart(this)" src="${image}"  alt="${valor}" data-valor="${index}" class="w-[150px] h-[200px]  shadow-md shadow-[#e9e9e9] border-[#bfe5fb] border-[1px] rounded-2xl hover:scale-[1.03] hover:shadow-black hover:shadow-1xl">`
         )
+
         console.log(`Cartas generadas`);     
         tableroGame.innerHTML = cartas.join("");
         console.log(`Cartas mostradas`);
@@ -107,13 +131,22 @@ const resultados = {
         (valor1+1 == valor2 || valor1 == valor2+1) ? hiddenCart(contendorDeCartas) : console.log(`cartas no pares`);
     };
 
+    function activarDesactivarCArta(valor,bool){
+        if(bool){
+            valor.classList.add("scale-[1.03]", "shadow-black", "shadow-1xl");
+        }else{
+         valor.classList.delete("scale-[1.03]", "shadow-black", "shadow-1xl");
+
+        }
+    }
+
     function addcart(valor){
         if (contendorDeCartas.length == 0){
-            valor.classList.add("scale-[1.03]", "shadow-black", "shadow-1xl");
+            activarDesactivarCArta(valor, true);
             contendorDeCartas.push(valor);
             console.log(`primera carta agregada`);
         }else{
-            valor.classList.add("scale-[1.03]", "shadow-black", "shadow-1xl");
+            activarDesactivarCArta(valor, true);
             contendorDeCartas.push(valor);
             console.log(`segunda carta agregada`);
             verificador(contendorDeCartas);
@@ -145,10 +178,8 @@ const resultados = {
             const modalSalir = document.getElementById("modalSalir");
             mostrarQuitarModal(modalSalir);
             mostrarQuitarModal(tableroGame);
-            playPauseShowHidden(true);
         }else{
             regresarJuego()
-            playPauseShowHidden(false)
         }
     }
 
@@ -157,20 +188,43 @@ const resultados = {
         mostrarQuitarModal(modalSalir);
         mostrarQuitarModal(tableroGame);
         playPause(cronometro);
-        playPauseShowHidden(false);
-    }        
-        //codigo para mostrar estadisticas
-        /*
-        setTimeout(()=>{
-            mostrarQuitarModal(modalGameOver);
-            mostrarQuitarModal(modalEstadisticas);
-        },4000)*/
+    }    
 
-//funcion para iniciar juego
+//funcion de primera etapa en modal
 
-    function starGAme() {
-        mostrarCartas("Arbol");
-        correrTiempo();
+    let inicio = {
+        avatar: ""
     }
 
-    starGAme();
+    function showAvatars () {
+        document.getElementById("avatares").innerHTML = avatares.map(element => `<img onclick="addPerfil(this)" src="${element}" class="w-[100px] overflow-auto rounded-2xl shadow-md hover:scale-[1.1] hover:shadow-lg hover:shadow-[#a3f9ff]">`).join("");
+        console.log(`Lista de avatares desplegado`);
+        
+    }
+
+    function starEtapa1(){
+        document.getElementById("tbn-star").style.display = "none"
+        console.log(`Inicio activo`);
+        showAvatars();
+        mostrarQuitarModal(document.getElementById("elejir-avatar"));
+    }
+    
+    function addPerfil(element){
+        inicio.avatar = element.src;
+        console.log(`avatar agregado`);
+        console.log(inicio.avatar);
+        starGame();
+    }
+
+    function mostrarAvatarDeUsuario(){
+        document.getElementById("avatar").src = `${inicio.avatar}`
+        console.log(`avatar actualizado`);
+    }
+
+//funcion para iniciar juego
+    function starGame() {
+        mostrarQuitarModal(document.getElementById("modaL-Inicio"));
+        mostrarAvatarDeUsuario()
+        mostrarCartas("Arbol",12);
+        correrTiempo();
+    }
